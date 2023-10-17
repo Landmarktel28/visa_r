28c66dd8ea3a2505f69e36ebb4387dd667c8d730
Mr Etim <etimvictordavid2@gmail.com>
2023-10-16 22:20:27 -0500

    deployment file added

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#Deploying a python web application 
+echo "Hello world"    = Shell script
+python ('Hello world')= Python script#Deploying a python web application 
+echo "Hello world"    = Shell script
+python ('Hello world')= Python script#Deploying a python web application 
+echo "Hello world"    = Shell script
+python ('Hello world')= Python script
